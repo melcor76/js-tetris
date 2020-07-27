17b84b8d66a33090abb9fc564d6817cd457506fc
Wonkyth <wonkyth@gmail.com>
2020-07-27 15:42:44 +1000

    fix event listener duplication

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const canvas = document.getElementById('board');
-const ctx = canvas.getContext('2d');
-const canvasNext = document.getElementById('next');
-const ctxNext = canvasNext.getContext('2d');
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+const canvasNext = document.getElementById("next");
+const ctxNext = canvasNext.getContext("2d");
+
+let eventListenersAttached = false;
 
 let accountValues = {
   score: 0,
   level: 0,
-  lines: 0
-}
+  lines: 0,
+};
 
 function updateAccount(key, value) {
   let element = document.getElementById(key);
@@ -21,18 +23,18 @@ let account = new Proxy(accountValues, {
     target[key] = value;
     updateAccount(key, value);
     return true;
-  }
+  },
 });
 
 let requestId;
 
 moves = {
-  [KEY.LEFT]: p => ({ ...p, x: p.x - 1 }),
-  [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
-  [KEY.DOWN]: p => ({ ...p, y: p.y + 1 }),
-  [KEY.SPACE]: p => ({ ...p, y: p.y + 1 }),
-  [KEY.UP]: p => board.rotate(p, ROTATION.RIGHT),
-  [KEY.Q]: p => board.rotate(p, ROTATION.LEFT)
+  [KEY.LEFT]: (p) => ({ ...p, x: p.x - 1 }),
+  [KEY.RIGHT]: (p) => ({ ...p, x: p.x + 1 }),
+  [KEY.DOWN]: (p) => ({ ...p, y: p.y + 1 }),
+  [KEY.SPACE]: (p) => ({ ...p, y: p.y + 1 }),
+  [KEY.UP]: (p) => board.rotate(p, ROTATION.RIGHT),
+  [KEY.Q]: (p) => board.rotate(p, ROTATION.LEFT),
 };
 
 let board = new Board(ctx, ctxNext);
@@ -47,7 +49,7 @@ function initNext() {
 }
 
 function addEventListener() {
-  document.addEventListener('keydown', event => {
+  document.addEventListener("keydown", (event) => {
     if (event.keyCode === KEY.P) {
       pause();
     }
@@ -84,7 +86,9 @@ function resetGame() {
 }
 
 function play() {
-  addEventListener();
+  if (!eventListenersAttached) {
+    addEventListener();
+  }
   resetGame();
   time.start = performance.now();
   // If we have an old game running a game then cancel the old
@@ -114,11 +118,11 @@ function animate(now = 0) {
 
 function gameOver() {
   cancelAnimationFrame(requestId);
-  ctx.fillStyle = 'black';
+  ctx.fillStyle = "black";
   ctx.fillRect(1, 3, 8, 1.2);
-  ctx.font = '1px Arial';
-  ctx.fillStyle = 'red';
-  ctx.fillText('GAME OVER', 1.8, 4);
+  ctx.font = "1px Arial";
+  ctx.fillStyle = "red";
+  ctx.fillText("GAME OVER", 1.8, 4);
 }
 
 function pause() {
@@ -130,9 +134,9 @@ function pause() {
   cancelAnimationFrame(requestId);
   requestId = null;
 
-  ctx.fillStyle = 'black';
+  ctx.fillStyle = "black";
   ctx.fillRect(1, 3, 8, 1.2);
-  ctx.font = '1px Arial';
-  ctx.fillStyle = 'yellow';
-  ctx.fillText('PAUSED', 3, 4);
+  ctx.font = "1px Arial";
+  ctx.fillStyle = "yellow";
+  ctx.fillText("PAUSED", 3, 4);
 }
